3f64986a0c646dd3079debe920e70902acdc92e0
Tawakoll <mohamedtawakol12@gmail.com>
2025-11-15 09:50:15 +0000

    distance calculated and shown in terminal successfully at day 2 !

--- src/distance_controller.cpp
+++ src/distance_controller.cpp
@@ -1,10 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/msg/pose.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-
-#define turtle1 1
-#define turtle2 2
-
+#include <cmath> // note to self don't need to add standard packages in cmake...
+#define TURTLE1 1
+#define TURTLE2 2
+#define TOLERANCE 0.2
 using std::placeholders::_1;
 class DistanceController: public rclcpp::Node
 {
@@ -27,25 +27,35 @@ class DistanceController: public rclcpp::Node
         private:
         void timer_callback()
         {   
-            // message.linear.x =2.0;
-            // message.angular.z=0.0;
-            // switch(turtle_choice)
-            // {
-            //     case turtle1: 
-            // // message.linear.x =2.0;
-            // // message.angular.z=0.0;          
-            //     break;
 
-            // }
-       
-                message.linear.x = 0.5;
-             message.angular.z = 0.0;
-            publisher_t1->publish(message);
-            publisher_t2->publish(message);
+            // basically calculate the distance between turtles 1&2 , by square root of the difference between x 1,2 and y1,2 positions.
+            dist_t1_t2 = std::sqrt(std::pow( (x_1-x_2) , 2 ) + std::pow( (y_1-y_2) , 2 )) ;
 
-
-
+            turtle_choice = TURTLE1; // gonna change to user input later
             
+            RCLCPP_INFO(this->get_logger(), "The distance between turtles =  '%f'",dist_t1_t2);
+
+             switch(turtle_choice)
+            {
+              case TURTLE1: 
+                        message.linear.x = 0.5;
+                        message.angular.z = 0.0;
+
+                        publisher_t1->publish(message);
+                break;
+
+                case TURTLE2:
+                //speed will be changed to user input
+                        message.linear.x = 0.5;
+                        message.angular.z = 0.0;
+                        publisher_t2->publish(message);    
+                break;
+
+                default:
+                //print error message
+                 RCLCPP_INFO(this->get_logger(), "The input for turtle choice  is invalid ");
+                 break;
+             }
 
         }
         void topic_callback(const turtlesim::msg::Pose::SharedPtr msg)
