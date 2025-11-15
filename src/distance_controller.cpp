39bf9eac5bb34ae8363eb53e15859b355bd1048f
Tawakoll <mohamedtawakol12@gmail.com>
2025-11-15 10:30:28 +0000

    distance is published also some adjustments to uiNode

--- src/distance_controller.cpp
+++ src/distance_controller.cpp
@@ -1,6 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/msg/pose.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/float32.hpp"
+
 #include <cmath> // note to self don't need to add standard packages in cmake...
 #define TURTLE1 1
 #define TURTLE2 2
@@ -20,6 +22,7 @@ class DistanceController: public rclcpp::Node
         subscription_t2 = this->create_subscription<turtlesim::msg::Pose>("turtle2/pose", 10,std::bind(&DistanceController::topic_callback_t2, this, _1));
         publisher_t2 = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 10);
         
+        distance_publisher = this->create_publisher<std_msgs::msg::Float32>("distance_t1_t2", 10);
 
         timer_ = this->create_wall_timer(std::chrono::milliseconds(50),std::bind(&DistanceController::timer_callback, this));
         }
@@ -30,6 +33,8 @@ class DistanceController: public rclcpp::Node
 
             // basically calculate the distance between turtles 1&2 , by square root of the difference between x 1,2 and y1,2 positions.
             dist_t1_t2 = std::sqrt(std::pow( (x_1-x_2) , 2 ) + std::pow( (y_1-y_2) , 2 )) ;
+            distance_message.data = dist_t1_t2;
+            distance_publisher-> publish(distance_message);
 
             turtle_choice = TURTLE1; // gonna change to user input later
             
@@ -80,9 +85,13 @@ class DistanceController: public rclcpp::Node
 
             rclcpp::TimerBase::SharedPtr timer_; // <-- Add Timer
             geometry_msgs::msg::Twist message;   // <-- Add message object
+
+
             float x_1 , x_2 , y_1, y_2, dist_t1_t2; // positions (x,y) for turtles 1&2 and dist_t1_t2 stores the distance between turtles 1 & 2.
             unsigned char turtle_choice ; // t_choice variable stores turtle choosen by the user , if == 1 , turtle1, else if == 2 turtle2, else invalid input. 
-
+            std_msgs::msg::Float32 distance_message; //message container for distance to be published
+           rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr distance_publisher ;// declare disntace publisher in the class
+       
 };
 int main(int argc, char * argv[])
 {
